f39681d9408fbd4a4359bbaf1e5ba4051f67c5bc
BalazsDev <nabali87@gmail.com>
2021-01-10 19:10:40 +0100

    GUI update

--- src/main/java/astro/Application.java
+++ src/main/java/astro/Application.java
@@ -13,6 +13,15 @@ public class Application extends JFrame{
 
     public JButton applicationBrowser;
     public JPanel applicationPanel;
+    private JButton blueButton;
+    private JButton greenButton;
+    private JButton redButton;
+    private JPanel red;
+    private JPanel green;
+    private JPanel blue;
+    private JPanel cardPanel;
+    private JPanel jPanel;
+    private JLayeredPane jLayeredPanel;
     public Component applicationsTab;
     public int row;
     public int column;
@@ -26,6 +35,45 @@ public class Application extends JFrame{
             public void actionPerformed(ActionEvent e) {
 
                 browse();
+
+            }
+        });
+        redButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                //cardPanel.removeAll();
+                cardPanel.add(red);
+                cardPanel.add(green);
+                cardPanel.add(blue);
+                red.setVisible(true);
+                green.setVisible(true);
+                blue.setVisible(true);
+                red.setBackground(new Color(100,100,100,100));
+                cardPanel.repaint();
+                cardPanel.revalidate();
+
+            }
+        });
+        greenButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                //cardPanel.removeAll();
+                cardPanel.remove(red);
+                cardPanel.repaint();
+                cardPanel.revalidate();
+
+            }
+        });
+        blueButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                cardPanel.removeAll();
+                cardPanel.add(blue);
+                cardPanel.repaint();
+                cardPanel.revalidate();
 
             }
         });
